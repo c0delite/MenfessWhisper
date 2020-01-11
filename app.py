a203ba5bd261c16f348c971da329d7636cd025c1
c0delite <budianto.andri92@gmail.com>
2020-01-11 15:02:03 +0700

    update fixed remove link

--- app.py
+++ app.py
@@ -1,50 +1,47 @@
 from twitter import Twitter
 import time
 
+#test pancing ke heroku supaya keluar dyno
+#test kedua pancing worker supaya muncul...
+
 tw = Twitter()
 def start():
-    print("Starting...")
+    print("Starting program...")
     dms = list()
     while True:
         if len(dms) is not 0:
-            print(len(dms))
             for i in range(len(dms)):
                 message = dms[i]['message']
                 sender_id = dms[i]['sender_id']
                 id = dms[i]['id']
 
-                if len(message) is not 0 and len(message) <= 500:
-                    if "https://" not in message and "http://" not in message:
-                        if "[pic]" in message:
-                            print("DM will be posting with media")
-                            tw.post_tweet_with_media(message, dms[i]['media'])
-                            tw.delete_dm(id)
 
-                        elif "[sender]" in message:
-                            message = message.replace("[sender]", "")
-                            screen_name = tw.get_user_screen_name(sender_id)
-                            media.download_image()
-                            media.process_image(message, screen_name)
-                            tw.post_tweet()
-                            tw.delete_dm(id)
-
-                        elif "[whisp]" in message:
-                            # message = message.replace("[whisp]","")
-                            print ("DM will post without image")
-                            tw.post_tweet_text(message)
-                            tw.delete_dm(id)
+                if len(message) is not 0 and len(message) < 280:
+                    if ".whisp" in message:
+                        if len(message) is not 0:
+                            if dms[i]['media'] is None:
+                                print("DM will be posted")
+                                tw.post_tweet(message)
+                                tw.delete_dm(id)
+                            else:
+                                print("DM will be posted with media")
+                                tw.download_image(dms[i]['media'])
+                                tw.post_tweet_with_media(message)
+                                tw.delete_dm(id)
 
                         else:
-                            media.download_image()
-                            media.process_image(message, None)
-                            tw.post_tweet()
+                            print("DM deleted because its empty..")
                             tw.delete_dm(id)
+
+
                     else:
+                        print("DM will be deleted because does not contains keyword..")
                         tw.delete_dm(id)
+
             dms = list()
 
         else:
-            print("DM is empty")
+            print("Direct message is empty...")
             dms = tw.read_dm()
             if len(dms) is 0:
                 time.sleep(60)
